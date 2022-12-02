576fbff6f3017c44047738c94bbc79765f1d9af1
Mr.Servo <mrservo@gmx.de>
2022-12-02 16:12:03 +0100

    [Weatherinfo] v1.4 tiny bugfix

--- Weatherinfo.py
+++ Weatherinfo.py
@@ -38,7 +38,7 @@ class Weatherinfo:
 						"RainSnowV2": ("5", "W"), "SnowShowersDayV2": ("16", "W"), "ThunderstormsV2": ("4", "0"),
 						"ClearNightV3": ("31", "C"), "MostlyClearNight": ("33", "C"), "PartlyCloudyNightV2": ("29", "I"),
 						"MostlyCloudyNightV2": ("27", "I"), "HazeSmokeNightV2_106": ("21", "K"), "HailNightV2": ("17", "X"),
-						"LightRainShowerNight": ("45", "Q"), "RainShowersNightV2": ("45", "R"), "N422Snow": ("14", "W")
+						"LightRainShowerNight": ("45", "Q"), "RainShowersNightV2": ("45", "R"), "N422Snow": ("14", "W"),
 						"RainSnowShowersNightV2": ("5", "W"), "SnowShowersNightV2": ("46", "W"), "na": ("NA", ")")
 						}  # mapping: msn -> (yahoo, meteo)
 		self.omwCodes = {"0": ("32", "B"), "1": ("34", "B"), "2": ("30", "H"), "3": ("28", "N"), "45": ("20", "M"), "48": ("21", "J"),
